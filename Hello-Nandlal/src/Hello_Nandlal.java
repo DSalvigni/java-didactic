bb21445ebcb4fb296d0da1e4832c8416ea3ce3c0
danie <danie@LAPTOP-P6AORBC6.fritz.box>
2019-06-26 15:33:30 +0200

    Addedd Message

--- Hello-Nandlal/src/Hello_Nandlal.java
+++ Hello-Nandlal/src/Hello_Nandlal.java
@@ -3,7 +3,7 @@ public class Hello_Nandlal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Nandlal");
+		System.out.println("Hello Nandlal, and good night");
 	}
 
 }
