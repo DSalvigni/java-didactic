9f3075aca6759120fa0b7e7f4ade87278b6da8e1
--:: SanGy ::-- <daniele.salvigni@gmail.com>
2019-06-26 16:18:12 +0200

    Finally the first commit from IntelliJ

--- Hello-Nandlal/src/Hello_Nandlal.java
+++ Hello-Nandlal/src/Hello_Nandlal.java
@@ -3,7 +3,7 @@ public class Hello_Nandlal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Nandlal, and good night!");
+		System.out.println("Hello Nandlal, ho are you doing?? and good night!");
 	}
 
 }
